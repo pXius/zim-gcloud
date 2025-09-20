0a5e627b144776f471102ad402158c3c78a28290
Hermanus Engelbrecht <hermanus.engelbrecht@sebx.se>
2025-09-20 22:22:25 +0200

    ZIMFW Compatible Completions

--- init.zsh
+++ init.zsh
@@ -1,6 +1,6 @@
-# Set CLOUDSDK_HOME if it is not already set.
 if [[ -z "${CLOUDSDK_HOME}" ]]; then
-  # Common locations for the Google Cloud SDK.
+  setopt local_options null_glob
+
   search_locations=(
     "$HOME/google-cloud-sdk"
     "/usr/local/share/google-cloud-sdk"
@@ -15,40 +15,84 @@ if [[ -z "${CLOUDSDK_HOME}" ]]; then
     "/opt/google-cloud-sdk"
     "/opt/google-cloud-cli"
     "/opt/local/libexec/google-cloud-sdk"
-    "$HOME/.asdf/installs/gcloud/*/"
+    "$HOME/.asdf/installs/gcloud"/*/
   )
 
-  # Set CLOUDSDK_HOME if a directory is found.
   for gcloud_sdk_location in "${search_locations[@]}"; do
     if [[ -d "${gcloud_sdk_location}" ]]; then
       CLOUDSDK_HOME="${gcloud_sdk_location}"
       break
     fi
   done
-  unset gcloud_sdk_location search_locations
 fi
 
-# If CLOUDSDK_HOME is set, source the necessary files.
 if [[ -n "${CLOUDSDK_HOME}" ]]; then
-  # Source the path file to add gcloud to the PATH.
+  export CLOUDSDK_HOME
+
   if [[ -f "${CLOUDSDK_HOME}/path.zsh.inc" ]]; then
     source "${CLOUDSDK_HOME}/path.zsh.inc"
   fi
 
-  # Possible locations
-  comp_files=(
-    "${CLOUDSDK_HOME}/completion.zsh.inc"             # Default location
-    "/usr/share/google-cloud-sdk/completion.zsh.inc"  # apt-based location
-  )
+  if [[ -z "${__GCLOUD_COMPLETION_LOADED:-}" ]]; then
+    __GCLOUD_COMPLETION_LOADED=1
 
-  # Loop through the possible locations and source the completion file if found.
-  for comp_file in "${comp_files[@]}"; do
-    if [[ -f "${comp_file}" ]]; then
-      source "${comp_file}"
-      break
-    fi
-  done
-  unset comp_file comp_files
+    _python_argcomplete() {
+        local prefix=
+        if [[ $COMP_LINE == 'gcloud '* ]]; then
+            if [[ $3 == ssh && $2 == *@* ]]; then
+                prefix=${2%@*}@
+                COMP_LINE=${COMP_LINE%$2}"${2#*@}"
+            elif [[ $2 == *'='* ]]; then
+                prefix=${2%=*}'='
+                COMP_LINE=${COMP_LINE%$2}${2/'='/' '}
+            fi
+        fi
+        local IFS=$'\v'
+        COMPREPLY=( $(IFS="$IFS" \
+            COMP_LINE="$COMP_LINE" \
+            COMP_POINT="$COMP_POINT" \
+            _ARGCOMPLETE_COMP_WORDBREAKS="$COMP_WORDBREAKS" \
+            _ARGCOMPLETE=1 \
+            "$1" 8>&1 9>&2 1>/dev/null 2>/dev/null) )
+        if [[ $? != 0 ]]; then
+            unset COMPREPLY
+            return
+        fi
+        if [[ ${#COMPREPLY[@]} == 1 && $COMPREPLY != *[=' '] ]]; then
+            COMPREPLY+=' '
+        fi
+        if [[ -n "$prefix" ]]; then
+            for i in {1..${#COMPREPLY[@]}}; do
+                COMPREPLY[$i-1]=$prefix${COMPREPLY[$i-1]}
+            done
+        fi
+    }
 
-  export CLOUDSDK_HOME
+    _completer() {
+        command=$1
+        name=$2
+        eval '[[ -n "$'"${name}"'_COMMANDS" ]] || '"${name}"'_COMMANDS="$('"${command}"')"'
+        set -- $COMP_LINE
+        shift
+        while [[ $1 == -* ]]; do
+              shift
+        done
+        [[ -n "$2" ]] && return
+        grep -q "${name}\s*$" <<< $COMP_LINE &&
+            eval 'COMPREPLY=($'"${name}"'_COMMANDS)' &&
+            return
+        [[ "$COMP_LINE" == *" " ]] && return
+        [[ -n "$1" ]] &&
+            eval 'COMPREPLY=($(echo "$'"${name}"'_COMMANDS" | grep ^'"$1"'))'
+    }
+
+    unset bq_COMMANDS
+    _bq_completer() {
+        _completer "CLOUDSDK_COMPONENT_MANAGER_DISABLE_UPDATE_CHECK=1 bq help | grep '^[^ ][^ ]*  ' | sed 's/ .*//'" bq
+    }
+
+    complete -o nospace -o default -F _python_argcomplete gcloud
+    complete -o default -F _bq_completer bq
+    complete -o nospace -F _python_argcomplete gsutil
+  fi
 fi
